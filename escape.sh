9e8066480c8ead998288e348b72f2506e08e65ea
vsr359 <srikanthreddy2210@gmail.com>
2022-05-14 15:55:00 -0700

    Revert "update echo -e"
    This reverts commit 190714f2f6d71a02a1c0c691a26e7374ad67ddca.

--- escape.sh
+++
@@ -1,7 +0,0 @@
-echo -e "Displaying \\ backlash:"
-echo -e "Making \a sound(alert):"
-echo -e "Dispalying \b backspace:"
-echo -e "Dispalying \' single quote:"
-echo -e "Displaying double \" quote:"
-echo -e "New \n Line:"
-
