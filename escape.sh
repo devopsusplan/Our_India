190714f2f6d71a02a1c0c691a26e7374ad67ddca
vsr359 <srikanthreddy2210@gmail.com>
2022-05-14 15:52:47 -0700

    update echo -e

---
+++ escape.sh
@@ -0,0 +1,7 @@
+echo -e "Displaying \\ backlash:"
+echo -e "Making \a sound(alert):"
+echo -e "Dispalying \b backspace:"
+echo -e "Dispalying \' single quote:"
+echo -e "Displaying double \" quote:"
+echo -e "New \n Line:"
+
